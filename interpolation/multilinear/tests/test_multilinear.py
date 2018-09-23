e6c75cfe9ae89432129f12717572d5355d096dff
Pablo Winant <pablo.winant@gmail.com>
2018-09-23 19:57:04 +0200

    TEST: Added test for get_index

--- interpolation/multilinear/tests/test_multilinear.py
+++ interpolation/multilinear/tests/test_multilinear.py
@@ -1,7 +1,23 @@
-
 from numpy import linspace, array
 from numpy.random import random
 from numba import typeof
+
+import numpy as np
+from interpolation.multilinear.fungen import get_index
+
+def test_barycentric_indexes():
+
+    # irregular grid
+    gg = np.array([0.0, 1.0])
+    assert(get_index(gg,-0.1) == (0,-0.1))
+    assert(get_index(gg,0.5) == (0, 0.5))
+    assert(get_index(gg,1.1) == (0, 1.1))
+
+    # regular grid
+    gg = (0.0,1.0,2)
+    assert(get_index(gg,-0.1) == (0,-0.1))
+    assert(get_index(gg,0.5) == (0,0.5))
+    assert(get_index(gg,1.1) == (0,1.1))
 
 
 # 2d-vecev-scalar
