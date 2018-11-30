f0a048d2d2315f693cedab7dfb4772aed9a610cb
Pablo Winant <pablo.winant@gmail.com>
2018-11-30 18:59:01 +0100

    Cubic prefilter implementation.

--- eval_cubic.py
+++ eval_cubic.py
@@ -5,9 +5,14 @@ from numpy import zeros
 from numpy import floor
 
 from codegen import txt, txt_vec, get_values, source_to_function
+from codegen import txt_1, txt_vec_1
 
 templ = tempita.Template(txt)
 templ_vec = tempita.Template(txt_vec)
+
+
+templ_1 = tempita.Template(txt_1)
+templ_1_vec = tempita.Template(txt_vec_1)
 
 #
 
@@ -78,6 +83,59 @@ def __eval_cubic(grid,C,points):
 def eval_cubic(*args):
     return _eval_cubic(*args)
 
+
+
+from numba import njit
+from numba.extending import overload
+
+def _eval_linear():
+    pass
+
+@overload(_eval_linear)
+def __eval_linear(grid,C,points,out):
+    d = len(grid)
+    n_x = len(grid.types)
+    vector_valued = (C.ndim==d+1)
+    vec_eval = (points.ndim==2)
+    from math import floor
+    from numpy import zeros
+    from numba import prange
+    context = {'floor': floor, 'zeros': zeros} #, 'Cd': Ad, 'dCd': dAd}
+
+    if vec_eval:
+        code = ( templ_1_vec.substitute(d=d, vector_valued=vector_valued, get_values=get_values, allocate=False) )[1:]
+    else:
+        code = ( templ_1.substitute(d=d, vector_valued=vector_valued, get_values=get_values, allocate=False) )[1:]
+    print(code)
+    f = source_to_function(code, context)
+    return f
+
+
+@overload(_eval_linear)
+def __eval_linear(grid,C,points):
+    d = len(grid)
+    n_x = len(grid.types)
+    vector_valued = (C.ndim==d+1)
+    vec_eval = (points.ndim==2)
+    from math import floor
+    from numpy import zeros
+
+    context = {'floor': floor, 'zeros': zeros} #, 'Cd': Ad, 'dCd': dAd}
+
+    if vec_eval:
+        code = ( templ_1_vec.substitute(d=d, vector_valued=vector_valued, get_values=get_values, allocate=True) )[1:]
+    else:
+        code = ( templ_1.substitute(d=d, vector_valued=vector_valued, get_values=get_values, allocate=True) )[1:]
+    print(code)
+    f = source_to_function(code, context)
+    return f
+
+@njit
+def eval_linear(*args):
+    """Do I get a docstring ?"""
+    return _eval_linear(*args)
+
+
 # print(templ.substitute(d=2, vector_valued=2, get_values=get_values, allocate=True))
 # print(templ.substitute(d=2, vector_valued=2, get_values=get_values, allocate=False))
 # print(templ.substitute(d=2, vector_valued=False, get_values=get_values, allocate=False))
@@ -94,11 +152,43 @@ grid = (
     (0.0, 1.0, 100),
     (0.0, 1.0, 100)
 )
+
+V = np.random.random((100,100))
+
 C = np.random.random((102,102))
-CC = np.random.random((102,102,2))
+
+VV = np.random.random((100,100,6))
+
+CC = np.random.random((102,102,6))
 points = np.random.random((N,2))
+
 out = np.random.random((N))
 Cout = np.random.random((N,2))
+
+
+eval_linear(grid, V, points, out)
+# eval_cubic(grid, C, points[0,:], out[0,0])
+eval_linear(grid, VV, points, Cout)
+
+eval_linear(grid, VV, points[0,:], Cout[0,:])
+
+
+from interpolation.multilinear.mlinterp import mlinterp
+%timeit mlinterp(grid, V,points)
+
+%timeit eval_linear(grid, V, points)
+%timeit eval_linear(grid, V, points)
+
+%timeit eval_linear(grid, V, points, out)
+
+
+
+eval_linear(grid, V, points[0,:])
+
+eval_linear(grid, VV, points)
+
+eval_linear(grid, VV, points[0,:])
+
 
 
 
@@ -140,14 +230,17 @@ vals = np.zeros((N,5))
 vals1 = np.zeros((N,5))
 from interpolation.splines.eval_cubic import eval_cubic_splines_3, vec_eval_cubic_splines_3
 
-eval_cubic_splines_3(a,b,n,CC,point,vval)
-eval_cubic(grid, CC, point, vval1)
+%timeit eval_cubic_splines_3(a,b,n,CC,point,vval)
+%timeit eval_cubic(grid, CC, point, vval1)
 abs(vval-vval1).max()
 
-vec_eval_cubic_splines_3(a,b,n,CC,points,vals)
-eval_cubic(grid, CC, points, vals1)
+%timeit vec_eval_cubic_splines_3(a,b,n,CC,points,vals)
+%timeit eval_cubic(grid, CC, points, vals1)
 dd = vals1-vals
 abs(dd[~np.isnan(dd)]).max()
+
+
+
 
 
 vvals2 = eval_cubic(grid,CC,point)
@@ -155,3 +248,14 @@ vals2 = eval_cubic(grid,CC,points)
 
 dd2 = vals2-vals
 abs(dd2).max()
+
+grid
+points
+
+
+V = np.random.random((K,K,K))
+
+from interpolation.multilinear.mlinterp import mlinterp
+%timeit mlinterp(grid, V, points)
+
+%timeit eval_cubic(grid, CC, points)
