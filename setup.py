d41b8bab49951ae283850fd673bebfd80c0a1cb5
Pablo Winant <pablo.winant@gmail.com>
2019-01-30 23:06:14 +0100

    Small version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages  # Always prefer setuptools over dis
 
 setup(
     name='interpolation',
-    version='2.0.1',
+    version='2.1.0',
     description='Interpolation in Python',
     url='https://github.com/econforge/interpolation.py',
     author='Chase Coleman, Spencer Lyon and Pablo Winant',
