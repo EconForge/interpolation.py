d623f1bbe7447c196b8e46d104daada0e1512ac1
Winant Pablo <pablo.winant@gmail.com>
2020-05-11 11:55:52 +0200

    FIX: compatibility with numba 0.47 and Anaconda.

--- interpolation/compat.py
+++ interpolation/compat.py
@@ -1,6 +1,4 @@
-
 from distutils.version import LooseVersion
-
 
 from numba import __version__
 
@@ -9,8 +7,9 @@ if LooseVersion(__version__)>='0.43':
 else:
     overload_options = {}
 
-from numba.core.types import Tuple, UniTuple
-# if LooseVersion(__version__)>='0.49':
-#     from numba.core.types import Tuple, UniTuple
-# else:
-#     from numba.core.types.containers import Tuple, UniTuple
+if LooseVersion(__version__)>='0.49':
+    from numba.core.types import Array, Float, Integer
+    from numba.core.types import Tuple, UniTuple
+else:
+    from numba.types import Array, Float, Integer
+    from numba.types.containers import Tuple, UniTuple
