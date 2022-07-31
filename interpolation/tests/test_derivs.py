3f61178867586cc321e43fd4df9eb4212ae9f79a
Mateo VG <mateovegi@gmail.com>
2022-07-31 12:23:01 -0400

    Test 1st order derivatives in 2d interps

--- interpolation/tests/test_derivs.py
+++ interpolation/tests/test_derivs.py
@@ -94,3 +94,96 @@ class Check1DDerivatives(unittest.TestCase):
         ).T
 
         self.assertTrue(np.allclose(grad, result, atol=0.02))
+
+
+class Check2DDerivatives(unittest.TestCase):
+    """ 
+    Checks derivatives in a 2D interpolator
+    """
+
+    def setUp(self):
+
+        pass
+
+    def test_linear(self):
+
+        # A linear function on a non-uniform grid
+        x = np.exp(np.linspace(0, 2, 6))
+        y = np.power(np.linspace(0, 5, 10), 2)
+
+        inter = 1
+        slope_x = 2
+        slope_y = -3
+        z = inter + slope_x * x[..., None] + slope_y * y[None, ...]
+
+        # Evaluation points
+        n_eval = 15
+        eval_points = np.vstack(
+            [np.linspace(-20, 20, n_eval), np.linspace(5, -5, n_eval)]
+        ).T
+
+        # Get function and 1st derivatives
+        grad = eval_spline(
+            CGrid(x, y),
+            z,
+            eval_points,
+            out=None,
+            order=1,
+            diff=str(((0, 0), (1, 0), (0, 1), (1, 1))),
+            extrap_mode="linear",
+        )
+
+        # 0-order must be the function
+        # (1,0) must be x slope
+        # (0,1) must be y slope
+        # (1,1) must be 0
+        result = np.hstack(
+            [
+                inter + slope_x * eval_points[:, 0:1] + slope_y * eval_points[:, 1:2],
+                np.ones((n_eval, 1)) * slope_x,
+                np.ones((n_eval, 1)) * slope_y,
+                np.zeros((n_eval, 1)),
+            ]
+        )
+
+        self.assertTrue(np.allclose(grad, result))
+
+    def test_nonlinear_approx(self):
+
+        # A non linear function on uniform grid
+        n_grid = 100
+        x = np.linspace(1, 5, n_grid)
+        y = np.linspace(1, 5, n_grid)
+        z = np.sin(x[..., None]) * np.log(y[None, ...])
+
+        # Evaluation points
+        n_eval = 15
+        eval_points = np.vstack(
+            [np.linspace(2, 4, n_eval), np.linspace(4, 2, n_eval)]
+        ).T
+
+        # Get function and 1st derivatives
+        grad = eval_spline(
+            CGrid(x, y),
+            z,
+            eval_points,
+            out=None,
+            order=1,
+            diff=str(((0, 0), (1, 0), (0, 1), (1, 1))),
+            extrap_mode="linear",
+        )
+
+        # 0-order must be sin(x)*ln(y)
+        # (1,0) must be cos(x) * ln(y)
+        # (0,1) must be sin(x) / y
+        # (1,1) must be cos(x) / y
+        result = np.hstack(
+            [
+                np.sin(eval_points[:, 0:1]) * np.log(eval_points[:, 1:2]),
+                np.cos(eval_points[:, 0:1]) * np.log(eval_points[:, 1:2]),
+                np.sin(eval_points[:, 0:1]) * (1 / eval_points[:, 1:2]),
+                np.cos(eval_points[:, 0:1]) * (1 / eval_points[:, 1:2]),
+            ]
+        )
+
+        self.assertTrue(np.allclose(grad, result, atol=0.02))
