3899b7694760217cd38f7510803264140636e432
Pablo Winant <pablo.winant@gmail.com>
2024-04-11 18:14:13 +0200

    Increased numba version.

--- interpolation/compat.py
+++ interpolation/compat.py
@@ -2,14 +2,7 @@ from packaging.version import parse
 
 from numba import __version__
 
-if parse(__version__) >= parse("0.43"):
-    overload_options = {"strict": False}
-else:
-    overload_options = {}
+overload_options = {"strict": False}
 
-if parse(__version__) >= parse("0.49"):
-    from numba.core.types import Array, Float, Integer
-    from numba.core.types import Tuple, UniTuple
-else:
-    from numba.types import Array, Float, Integer
-    from numba.types.containers import Tuple, UniTuple
+from numba.core.types import Array, Float, Integer
+from numba.core.types import Tuple, UniTuple
